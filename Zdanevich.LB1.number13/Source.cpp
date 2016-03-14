b3ae582bf7058716a9469f4f8fbc5a710ed30ccf
Oleg Zdanevic <ya.zdanevich@yandex.ru>
2016-03-14 22:23:56 +0300

    Дописал защиту от ввода символьного типа в целочисленный тип

--- Zdanevich.LB1.number13/Source.cpp
+++ Zdanevich.LB1.number13/Source.cpp
@@ -1,50 +1,48 @@
 #include <iostream>;
 #include <string.h>;
-#define _IGNORE_BLOCK
 using namespace std;
 
+int EnterDimention();
+int FromCharToInt(char*);
 short ThanslateFromHex(char);
-int* CreatingArray(int );
-void EnterHexSign(char& );
+int* CreatingArray(int);
+void EnterHexSign(char&);
 void DeleteElementsWithSpecialSign(int *, int&, short);
-void QuickSort(int *, int );
-void QuickSort(int *,int *, int , int );
-void TakeFirstSymboFromHex(int *, int *, int );
+void QuickSort(int *, int);
+void QuickSort(int *, int *, int, int);
+void TakeFirstSymboFromHex(int *, int *, int);
 void Swap(int &, int &);
 void DeleteTheSameElements(int *, int &);
-void ShiftArray(int *, int , int *);
-void EnterDimention(int &); 
-void ChoiceFillingStyle(int *,int );
-void EnterArrayYourself(int *,int );
-void EnterArrayRandom(int *, int );
-void DisplayArray(int *, int );
-bool IsDataValid(int );
-bool IsChoiceValid(int );
+void ShiftArray(int *, int, int *);
+void ChoiceFillingStyle(int *, int);
+void EnterArrayYourself(int *, int);
+void EnterArrayRandom(int *, int);
+void DisplayArray(int *, int);
+bool IsSymbol(char*);
+bool IsChoiceValid(int);
 bool IsHexSymbolValid(char*);
+bool IsDataValid(int);
 char ToUpper(char);
 
 
 
 int main()
 {
-	
-	
+	int n = 0;
 	while (true)
 	{
 		cout.width(50);
 		cout << endl << "Welcome to the programm!";
 		cout << endl << endl << "*******************************************************************************" << endl << endl;
 		cout << endl << "This programm is sorting numbers by their first digit" << endl << "in their hex form (in descending order)" << endl;
-		system("pause");
-		system("cls");
-		int n;
-		EnterDimention(n);
+		if (n == 0) cout << "Press Enter, please..." << endl;
+		n = EnterDimention();
 		system("cls");
 		int *a = CreatingArray(n);
-		ChoiceFillingStyle(a,n);
+		ChoiceFillingStyle(a, n);
 		DisplayArray(a, n);
-		cout << "-array before sorting"<<endl;
-		DeleteTheSameElements(a,n);
+		cout << "-array before sorting" << endl;
+		DeleteTheSameElements(a, n);
 		QuickSort(a, n);
 		DisplayArray(a, n);
 		cout << "-array after sorting" << endl;
@@ -69,20 +67,64 @@ int main()
 		cout << "No- other symbols" << endl;
 		cout << "Waiting for Enter: " << endl;
 		cin >> s;
-		system("cls"); 
+		system("cls");
 		a = NULL;
-		delete [] a;
+		delete[] a;
 		if (s == 'y' || s == 'Y') continue;
 		break;
 
 	}
-	
+
 	return 0;
+}
+
+int EnterDimention()
+{
+	getchar();
+	char *n = new char[255];
+	cout << "How many numbers do you want to sort?" << endl;
+	while (true)
+	{
+		cout << "Enter size   0<=n    : ";
+		cin.getline(n, 255, '\n');
+		if (!IsSymbol(n))
+		{
+			int x = FromCharToInt(n);
+			if (IsDataValid(x))
+			{
+				return x;
+			}
+
+		}
+		cout << "Error!" << endl;
+	}
+}
+
+int FromCharToInt(char*n)
+{
+	int len = strlen(n);
+	len--;
+	int q = 1;
+	int num, digit;
+	num = digit = 0;
+	for (int i = len; i >= 0; i--)
+	{
+		if (i == 0 && n[0] == '-')
+		{
+			num *= (-1);
+			break;
+		}
+		digit = n[i] - '0';
+		digit *= q;
+		num += digit;
+		q *= 10;
+	}
+	return num;
 }
 
 short ThanslateFromHex(char s)
 {
-	
+
 	switch (s)
 	{
 	case 'A':
@@ -116,7 +158,7 @@ void EnterHexSign(char& a)
 		a = ToUpper(a);
 		char*s = &a;
 		if (IsHexSymbolValid(s)) break;
-		cout << "Error!"<<endl;
+		cout << "Error!" << endl;
 		system("pause");
 		system("cls");
 	}
@@ -127,9 +169,20 @@ void DeleteElementsWithSpecialSign(int *a, int &n, short k)
 	int   l;
 	short q;
 
-	for (int *i = a; i < a+n; i++)
+	for (int *i = a; i < a + n; i++)
 	{
-		l = abs(*i);
+		l = *i;
+		if (l < 0)
+		{
+			q = 15;
+			if (q == k)
+			{
+				ShiftArray(a, n, i);
+				i--;
+				n--;
+			}
+			continue;
+		}
 		while (l >= 1)
 		{
 			q = l % 16;
@@ -149,17 +202,17 @@ void QuickSort(int *a, int n)
 {
 	int *arr = CreatingArray(n);
 	TakeFirstSymboFromHex(a, arr, n);
-	QuickSort(arr,a, 0, n-1);
+	QuickSort(arr, a, 0, n - 1);
 	arr = NULL;
-	delete [] arr;
+	delete[] arr;
 }
 
-void QuickSort(int *arr,int*b, int left, int right) 
+void QuickSort(int *arr, int*b, int left, int right)
 {
 	int i = left, j = right;
 	int pivot = arr[(left + right) / 2];
 
-	
+
 	while (i <= j) {
 		while (arr[i] > pivot)
 			i++;
@@ -174,21 +227,26 @@ void QuickSort(int *arr,int*b, int left, int right)
 	}
 
 	if (left < j)
-		QuickSort(arr,b, left, j);
+		QuickSort(arr, b, left, j);
 	if (i < right)
-		QuickSort(arr,b, i, right);
+		QuickSort(arr, b, i, right);
 
 
 }
 
-void TakeFirstSymboFromHex(int *a,int *b, int n)
+void TakeFirstSymboFromHex(int *a, int *b, int n)
 {
-	int  q,l;
-	
+	int  q, l;
+
 	for (int i = 0; i < n; i++)
 	{
-		l = abs(a[i]);
-		while (l>=1)
+		l = a[i];
+		if (l < 0)
+		{
+			b[i] = 15;
+			continue;
+		}
+		while (l >= 1)
 		{
 			q = l % 16;
 			l /= 16;
@@ -208,14 +266,14 @@ void Swap(int &x, int &y)
 void DeleteTheSameElements(int *a, int &n)
 {
 	int *i, *j;
-	for ( i = a; i < a+n; i++) 
+	for (i = a; i < a + n; i++)
 	{
-		for (j = i + 1; j < a+n; j++) 
+		for (j = i + 1; j < a + n; j++)
 		{
-			if (*i == *j) 
+			if (*i == *j)
 			{
 				ShiftArray(a, n, j);
-				n = n--; 
+				n = n--;
 				*j = *j--;
 			}
 		}
@@ -224,50 +282,55 @@ void DeleteTheSameElements(int *a, int &n)
 
 void ShiftArray(int *a, int n, int *j)
 {
-	for (int* k = j; k <= a+n; k++)
+	for (int* k = j; k <= a + n; k++)
 	{
-		*k = *(k+1); 
+		*k = *(k + 1);
 	}
 }
 
-void EnterDimention(int &n)
+void ChoiceFillingStyle(int *a, int n)
 {
-	cout << "How many numbers do you want to sort?" << endl;
+	char *s = new char[255];
 	while (true)
 	{
-		cout << "Enter size   0<=n    : ";
-		cin >> n;
-		if (IsDataValid(n)) break;
-		cout << "Error!" << endl;
-	}
-}
+		cout << "How do you want to fill the array?" << endl;
+		cout << "1) by yourself" << endl;
+		cout << "2) random" << endl;
+		cin.getline(s, 255, '\n');
+		if (!IsSymbol(s))
+		{
+			int x = FromCharToInt(s);
+			system("cls");
+			if (IsChoiceValid(x))
+			{
 
-void ChoiceFillingStyle(int *a,int n)
-{
-	cout << "How do you want to fill the array?" << endl;
-	cout << "1) by yourself" << endl;
-	cout << "2) random" << endl;
-	int k;
-	while (true)
-	{
-		cout << "Waiting for enter: ";
-		cin >> k;
-		if (IsChoiceValid(k)) break;
+				if (x == 1) EnterArrayYourself(a, n);
+				else EnterArrayRandom(a, n);
+				break;
+			}
+		}
 		cout << "Error!" << endl;
 	}
-	system("cls");
-	if (k == 1) EnterArrayYourself(a,n);
-	else EnterArrayRandom(a, n);
 }
 
 void EnterArrayYourself(int *a, int n)
 {
 	cout << "Warning! You should enter integer numbers," << endl << "If you enter 3.4, it will be 3 in programm," << endl << "Please, be careful:" << endl;
-	short k = 1;
-	for (int *i = a; i < a+n; i++)
+	int k = 1;
+	char *s = new char[255];
+	for (int *i = a; i < a + n; i++)
 	{
-		cout << "Element" << "[" << k << "]  :  ";
-		cin >> *i;
+		while (true)
+		{
+			cout << "Element" << "[" << k << "]  :  ";
+			cin.getline(s, 255, '\n');
+			if (!IsSymbol(s))
+			{
+				*i = (FromCharToInt(s));
+				break;
+			}
+			cout << "Error!" << endl;
+		}
 		k++;
 	}
 	system("cls");
@@ -276,21 +339,27 @@ void EnterArrayYourself(int *a, int n)
 
 void EnterArrayRandom(int *a, int n)
 {
-	for (int *i = a; i < a+n; i++)
+	for (int *i = a; i < a + n; i++)
 	{
-	    *i=rand();
+		*i = rand();
 	}
 }
 
 void DisplayArray(int *a, int n)
 {
-	for (int *i = a; i < a+n; i++)
+	for (int *i = a; i < a + n; i++)
 		cout << *i << " ";
 }
 
-bool IsDataValid(int n)
+bool IsSymbol(char *n)
 {
-	if (n <= 0) return false;
+
+	const char *alphebet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM~`!@#$%^&*()_|+=-{}[]:;'\<>,.?/";
+	char *q = strpbrk(n, alphebet);
+	if (q == NULL) return false;
+	const char *minus = "-";
+	q = strpbrk(n, minus);
+	if (q == n) return false;
 	return true;
 }
 
@@ -303,9 +372,15 @@ bool IsChoiceValid(int n)
 bool IsHexSymbolValid(char *a)
 {
 	char *test = "ABCDEF";
-	char *q = strpbrk(a,test);
+	char *q = strpbrk(a, test);
 	if (q != NULL) return true;
 	return false;
+}
+
+bool IsDataValid(int n)
+{
+	if (n <= 0) return false;
+	return true;
 }
 
 char ToUpper(char symbol)
